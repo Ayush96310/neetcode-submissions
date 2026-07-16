1f2237fdc23f2c2f3b87bb0c95e3d9d2ac9f282d
AYUSH <138558694+Ayush96310@users.noreply.github.com>
2026-07-16 23:36:18 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int  n =  nums.size();
+        int prev1 = 0;
+        int prev2 = 0;
+        for(int i=0; i<n;i++){
+            int current = max(prev1,prev2+nums[i]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return max(prev1,prev2);
+    }
+};
